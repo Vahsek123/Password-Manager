c3fe0a7033001ecd264193a2a8362c7d77e0098f
Vahsek123 <68935747+Vahsek123@users.noreply.github.com>
2020-12-10 16:01:57 +1300

    Implemented text-based program
    Can now add/view passwords

--- main.py
+++ main.py
@@ -1,23 +1,50 @@
 import mysql.connector as sql
 from tokens import get_token
+from tabulate import tabulate
 
 
-mydb = sql.connect(
-    host='localhost',
-    user='root',
-    password=get_token(),
-    database='password_manager'
-)
+def main():
+    # connect to SQL database
+    mydb = sql.connect(
+        host='localhost',
+        user='root',
+        password=get_token(),
+        database='password_manager'
+    )
+    cursor = mydb.cursor()
 
-mycursor = mydb.cursor()
+    while True:
 
-ins = "INSERT INTO password_vault(password, app, url) " \
-      "VALUES(AES_ENCRYPT(%s,UNHEX(SHA2('passkey', 224))), %s, %s)"
+        add_view = input("Do you want to add or view passwords (1/2): ")
 
-input_pass = input("Enter password to store: ")
-val = (input_pass, 'Tester', 'https://tester.com')
+        if add_view == '1':  # add password
+            password = input("Enter password to store: ")
+            app = input("Enter the application this is for: ")
+            url = input("What is the url for the application: ")
+
+            # insert encrypted password into vault
+            ins = "INSERT INTO password_vault(password, app, url) " \
+                  "VALUES(AES_ENCRYPT(%s,UNHEX(SHA2('passkey', 224))), %s, %s)"
+            val = (password, app, url)
+            cursor.execute(ins, val)
+            mydb.commit()
+
+        else:  # view current passwords
+            cursor.execute("SELECT * FROM password_vault")
+            result = cursor.fetchall()
+            print(tabulate(result, headers=['pass_id', 'password', 'app', 'url'], tablefmt='psql'))
+
+            # get encrypted password
+            pass_id = (int(input("Which password do you want to view: ")),)
+            get_val = "SELECT @val := password FROM password_vault WHERE pass_id = %s;"
+            cursor.execute(get_val, pass_id)
+            encrypted_pass = cursor.fetchone()
+
+            # decrypt password and print to console
+            get_pass = "SELECT AES_DECRYPT(%s, UNHEX(SHA2('passkey', 224)));"
+            cursor.execute(get_pass, encrypted_pass)
+            output = ''.join(cursor.fetchone())
+            print(output)
 
 
-mycursor.execute(ins, val)
-mydb.commit()
-
+main()
